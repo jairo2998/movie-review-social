90c8671470d5e3dcaf1e0d1bb225b0f8c9951846
Jairo Mendes <jairo2998@gmail.com>
2025-11-21 12:11:53 -0300

    feat: Implement logic to save user reviews/critiques to database

--- review_process.php
+++ review_process.php
@@ -7,6 +7,7 @@
   require_once("models/Review.php");
   require_once("dao/UserDAO.php");
   require_once("dao/ReviewDAO.php");
+  require_once("dao/MovieDAO.php");
 
   // Verificando o tipo do formulário
   $type = filter_input(INPUT_POST, "type");
@@ -14,6 +15,7 @@
   $message = new Message($BASE_URL);
   $auth = new UserDAO($conn, $BASE_URL);
   $reviewDao = new ReviewDAO($conn, $BASE_URL);
+  $movieDao = new MovieDAO($conn, $BASE_URL);
 
   // Pegar dados do usuário
   $userData = $auth->verifyToken();
@@ -26,35 +28,31 @@
     // Recebendo os inputs do formulário
     $rating = filter_input(INPUT_POST, "rating");
     $review = filter_input(INPUT_POST, "review");
-    $movies_id = filter_input(INPUT_POST, "movies_id");
+    $movies_id = filter_input(INPUT_POST, "movie_id");
 
     $reviewObject = new Review();
-    $movieData = $movieDao->findById($id);
+    $movieData = $movieDao->findById($movies_id);
+    
+    if($movieData){   
 
-    if($movieData){
+        // Validação de dados mínimos
+        if(!empty($rating) && 
+            !empty($review) &&
+            !empty($movies_id)) {
+            $reviewObject->rating = $rating;
+            $reviewObject->review = $review;
+            $reviewObject->movies_id = $movies_id;
+            $reviewObject->users_id = $userData->id;
 
-    } else {
+            $reviewDao->create($reviewObject);
+
+        } else {
+
         $message->setMessage("Você precisa adicionar a sua nota e o comentário.", "error", "back");
-    }
 
-    // Validação de dados mínimos
-    if(!empty($rating) && 
-      !empty($review) &&
-      !empty($movies_id)) {
-
-        $reviewObject->rating = $rating;
-        $reviewObject->review = $review;
-        $reviewObject->movies_id = $movies_id;
-        $reviewObject->users_id = $userData->id;
-
-        $reviewDao->create($reviewObject);
+        }
 
     } else {
-
-      $message->setMessage("Você precisa adicionar a sua nota e o comentário.", "error", "back");
-
+        $message->setMessage("Informações inválidas.", "error", "index.php");
     }
-
-  } else {
-    $message->setMessage("Informações inválidas.", "error", "index.php");
-  }
+}
